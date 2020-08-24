a20cfbf51e4191267dabffa65b20b762eea112fc
EvgenyNarel <narel40@mail.ru>
2020-08-24 22:30:51 +0300

    Lesson_10 Text corrected "\\p{P}" ==>  "\\p{P}|\\s+"

--- src/task_10/Text.java
+++ src/task_10/Text.java
@@ -4,18 +4,19 @@ import java.util.*;
 
 /*
 Имеется текст. Следует составить для него частотный словарь.
+
  */
 public class Text {
     public static void main(String[] args) {
-        String stroka = "Вот дом, Который построил Джек. А это пшеница, Которая в темном чулане хранится В доме, " +
-                "Который построил Джек. А это веселая птица-синица, Которая часто ворует пшеницу, Которая в темном чулане хранится В доме," +
-                " Который построил Джек.";
+        String stroka = "test,test,word,sentence      words number  is   eight";
 
-        String i[] = stroka.split(" ");
+
+        String i[] = stroka.split("\\p{P}|\\s+");
 
         LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
 
         for (String t : i) {
+
             if (map.containsKey(t)) {       //проверяем имеется ли такой ключ в mape если да то
                 map.put(t, map.get(t) + 1); // добавляем только значение, а map.get(t) +1  - возвращает значение объекта, ключ которого равен t и добавляет 1
 
@@ -33,7 +34,7 @@ public class Text {
             System.out.print(key);//выводим ключ
             System.out.println(" : " + map.get(key));//выводим значение
         }
-            System.out.println("Уникальных слов: " + counter);
+        System.out.println("Уникальных слов: " + counter);
     }
 }
 
