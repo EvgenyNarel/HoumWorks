f2a8fec51d82592fdceb4c29ea4e7b632bab2dd1
EvgenyNarel <67507704+EvgenyNarel@users.noreply.github.com>
2020-09-06 19:45:22 +0300

    Delete RunnerDataTypes.java

--- src/Task_9/Tayps/RunnerDataTypes.java
+++
@@ -1,8 +0,0 @@
-package Task_9.Tayps;
-
-public class RunnerDataTypes {
-    public static void main(String[] args) {
-        DataTypes one = new DataTypes();
-        one.getContent(-1);
-    }
-}
